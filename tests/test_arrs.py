f16c0589ff644117114d3ea9c23b2c536ce63a20
Olga Yarosh <O_ll_y_a@mail.ru>
2023-02-18 21:41:29 +0300

    add test_get_negative, test_slice_empty, test_slice_negative_start

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,19 @@ def test_get():
     assert arrs.get([], 0, "test") == "test"
 
 
+def test_get_negative():
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
+
+
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+
+def test_slice_empty():
+    assert arrs.my_slice([], 1, 3) == []
+
+
+def test_slice_negative_start():
+    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
+
